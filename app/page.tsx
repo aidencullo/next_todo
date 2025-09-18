21eea18ff1b92a2b322d456961c3abc96e03f47b
Aiden Cullo <aidencullo@gmail.com>
2025-09-18 09:47:31 -0400

    Add egg management functionality with useState

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const addEgg = () => {
+    setEggs([...eggs, 'egg']);
+  }
+
+  const removeEgg = () => {
+    setEggs(eggs.slice(0, -1));
+  }
+
+  const [eggs, setEggs] = useState<string[]>([]);
   return (
     <div>
-      <h1>hello world</h1>
+      <h1>goobye world</h1>
+      <div className="flex flex-col gap-2">
+        <button onClick={addEgg}>add egg</button>
+        <button onClick={removeEgg}>remove egg</button>
+      </div>
+      <ul>
+        {eggs.map((egg, index) => 
+          <li key={index}>{egg}</li>
+        )}
+      </ul>
     </div>
   );
 }
